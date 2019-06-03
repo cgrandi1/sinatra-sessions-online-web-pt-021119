6ef7c79dbc71ebfef68173858aca5a42082103d2
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-03 03:29:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
-
+    binding.pry
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
